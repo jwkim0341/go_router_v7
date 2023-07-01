98fe83e6bc463d3a73fb5922cf69c3fe82a3cb96
jwkim0341 <92710923+jwkim0341@users.noreply.github.com>
2023-07-01 09:14:05 +0900

    go 함수 사용해보기

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,17 +1,28 @@
 import 'package:go_router/go_router.dart';
+import 'package:go_router_v7/screens/1_basic_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 // https://blog.codefactory.ai == / -> path
 // https://blog.codefactory.ai/flutter -> /flutter
+// / -> home
+// /basic -> basic screen
 final router = GoRouter(
   routes: [
     GoRoute(
       // 기본 페이지 설정 (home)
       path: '/',
       //화면지정 - 이동할 페이지 반환
-      builder: (context,state){
+      builder: (context, state) {
         return RootScreen();
       },
+      routes: [
+        GoRoute(
+          path: 'basic',
+          builder: (context, state) {
+            return BasicScreen();
+          },
+        ),
+      ],
     ),
   ],
 );
