61ec968bdecbc50498bd397f9c74b98a185e1d79
jwkim0341 <92710923+jwkim0341@users.noreply.github.com>
2023-07-01 09:57:55 +0900

    pop 함수 사용해보기

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_v7/screens/1_basic_screen.dart';
 import 'package:go_router_v7/screens/3_push_screen.dart';
+import 'package:go_router_v7/screens/4_pop_base_screen.dart';
+import 'package:go_router_v7/screens/5_pop_return_screen.dart';
 import 'package:go_router_v7/screens/root_screen.dart';
 
 // https://blog.codefactory.ai == / -> path
@@ -35,6 +37,22 @@ final router = GoRouter(
           builder: (context, state) {
             return PushScreen();
           },
+        ),
+        GoRoute(
+          path: 'pop',
+          builder: (context, state) {
+            // /pop
+            return PopBaseScreen();
+          },
+          routes: [
+            GoRoute(
+              path: 'return',
+              builder: (context, state) {
+                // /pop/return
+                return PopReturnScreen();
+              },
+            ),
+          ],
         ),
       ],
     ),
